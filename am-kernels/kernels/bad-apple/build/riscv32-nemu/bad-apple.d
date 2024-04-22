2cc2bc05d8dd1e221ea434e9c16ec64c8e0a6285
tracer-ics2023 <tracer@njuics.org>
2024-04-22 19:36:07 +0800

    >  compile NEMU 221220000 张三 Linux rookiemaster-ubuntu 6.5.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 12 10:22:43 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:36:07 up 58 min,  1 user,  load average: 1.35, 0.52, 0.23

---
+++ am-kernels/kernels/bad-apple/build/riscv32-nemu/bad-apple.d
@@ -0,0 +1,6 @@
+/home/rookiemaster/ics2023/am-kernels/kernels/bad-apple/build/riscv32-nemu/bad-apple.o: \
+ /home/rookiemaster/ics2023/am-kernels/kernels/bad-apple/bad-apple.c \
+ /home/rookiemaster/ics2023/abstract-machine/am/include/am.h \
+ /home/rookiemaster/ics2023/abstract-machine/am/include/arch/riscv.h \
+ /home/rookiemaster/ics2023/abstract-machine/am/include/amdev.h \
+ /home/rookiemaster/ics2023/abstract-machine/klib/include/klib-macros.h
---
+++ am-kernels/kernels/bad-apple/build/riscv32-nemu/resources.d
@@ -0,0 +1,2 @@
+/home/rookiemaster/ics2023/am-kernels/kernels/bad-apple/build/riscv32-nemu/resources.o: \
+ /home/rookiemaster/ics2023/am-kernels/kernels/bad-apple/resources.S
