40d5e11ed7259c32523be71f886db056cb505f88
tracer-ics2023 <tracer@njuics.org>
2024-04-22 21:20:05 +0800

    >  compile NEMU 221220000 张三 Linux rookiemaster-ubuntu 6.5.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 12 10:22:43 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:20:05 up  2:42,  1 user,  load average: 0.74, 0.38, 0.33

--- am-kernels/tests/am-tests/src/main.c
+++ am-kernels/tests/am-tests/src/main.c
@@ -16,6 +16,8 @@ static const char *tests[256] = {
 };
 
 int main(const char *args) {
+  printf("%s","hhh");
+
   switch (args[0]) {
     CASE('h', hello);
     CASE('i', hello_intr, IOE, CTE(simple_trap));
