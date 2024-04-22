f6442360acc9ad60fe7c5c8819f23bb9e9c9787f
tracer-ics2023 <tracer@njuics.org>
2024-04-22 21:08:54 +0800

    >  compile NEMU 221220000 张三 Linux rookiemaster-ubuntu 6.5.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 12 10:22:43 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:08:54 up  2:31,  1 user,  load average: 0.06, 0.31, 0.41

---
+++ am-kernels/tests/am-tests/build/riscv32-nemu/src/tests/audio/audio-data.d
@@ -0,0 +1,2 @@
+/home/rookiemaster/ics2023/am-kernels/tests/am-tests/build/riscv32-nemu/src/tests/audio/audio-data.o: \
+ /home/rookiemaster/ics2023/am-kernels/tests/am-tests/src/tests/audio/audio-data.S
---
+++ am-kernels/tests/am-tests/build/riscv32-nemu/src/tests/mp.d
@@ -0,0 +1,8 @@
+/home/rookiemaster/ics2023/am-kernels/tests/am-tests/build/riscv32-nemu/src/tests/mp.o: \
+ /home/rookiemaster/ics2023/am-kernels/tests/am-tests/src/tests/mp.c \
+ /home/rookiemaster/ics2023/am-kernels/tests/am-tests/include/amtest.h \
+ /home/rookiemaster/ics2023/abstract-machine/am/include/am.h \
+ /home/rookiemaster/ics2023/abstract-machine/am/include/arch/riscv.h \
+ /home/rookiemaster/ics2023/abstract-machine/am/include/amdev.h \
+ /home/rookiemaster/ics2023/abstract-machine/klib/include/klib.h \
+ /home/rookiemaster/ics2023/abstract-machine/klib/include/klib-macros.h
