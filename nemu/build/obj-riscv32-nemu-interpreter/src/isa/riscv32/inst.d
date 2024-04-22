d139b20d9ec981b6bad4990dc6383b7a1ec35f54
tracer-ics2023 <tracer@njuics.org>
2024-04-22 21:02:19 +0800

    >  compile NEMU 221220000 张三 Linux rookiemaster-ubuntu 6.5.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 12 10:22:43 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:02:19 up  2:25,  1 user,  load average: 1.02, 0.47, 0.47

--- nemu/build/obj-riscv32-nemu-interpreter/src/isa/riscv32/inst.d
+++ nemu/build/obj-riscv32-nemu-interpreter/src/isa/riscv32/inst.d
@@ -1,29 +1,29 @@
-cmd_/home/admin/ics2023/nemu/build/obj-riscv32-nemu-interpreter/src/isa/riscv32/inst.o := unused
+cmd_/home/rookiemaster/ics2023/nemu/build/obj-riscv32-nemu-interpreter/src/isa/riscv32/inst.o := unused
 
-source_/home/admin/ics2023/nemu/build/obj-riscv32-nemu-interpreter/src/isa/riscv32/inst.o := src/isa/riscv32/inst.c
+source_/home/rookiemaster/ics2023/nemu/build/obj-riscv32-nemu-interpreter/src/isa/riscv32/inst.o := src/isa/riscv32/inst.c
 
-deps_/home/admin/ics2023/nemu/build/obj-riscv32-nemu-interpreter/src/isa/riscv32/inst.o := \
+deps_/home/rookiemaster/ics2023/nemu/build/obj-riscv32-nemu-interpreter/src/isa/riscv32/inst.o := \
   src/isa/riscv32/local-include/reg.h \
     $(wildcard include/config/rt/check.h) \
     $(wildcard include/config/rve.h) \
-  /home/admin/ics2023/nemu/include/common.h \
+  /home/rookiemaster/ics2023/nemu/include/common.h \
     $(wildcard include/config/target/am.h) \
     $(wildcard include/config/mbase.h) \
     $(wildcard include/config/msize.h) \
     $(wildcard include/config/isa64.h) \
-  /home/admin/ics2023/nemu/include/macro.h \
-  /home/admin/ics2023/nemu/include/debug.h \
-  /home/admin/ics2023/nemu/include/utils.h \
+  /home/rookiemaster/ics2023/nemu/include/macro.h \
+  /home/rookiemaster/ics2023/nemu/include/debug.h \
+  /home/rookiemaster/ics2023/nemu/include/utils.h \
     $(wildcard include/config/target/native/elf.h) \
-  /home/admin/ics2023/nemu/include/cpu/cpu.h \
-  /home/admin/ics2023/nemu/include/cpu/ifetch.h \
-  /home/admin/ics2023/nemu/include/memory/vaddr.h \
-  /home/admin/ics2023/nemu/include/cpu/decode.h \
+  /home/rookiemaster/ics2023/nemu/include/cpu/cpu.h \
+  /home/rookiemaster/ics2023/nemu/include/cpu/ifetch.h \
+  /home/rookiemaster/ics2023/nemu/include/memory/vaddr.h \
+  /home/rookiemaster/ics2023/nemu/include/cpu/decode.h \
     $(wildcard include/config/itrace.h) \
-  /home/admin/ics2023/nemu/include/isa.h \
-  /home/admin/ics2023/nemu/src/isa/riscv32/include/isa-def.h \
+  /home/rookiemaster/ics2023/nemu/include/isa.h \
+  /home/rookiemaster/ics2023/nemu/src/isa/riscv32/include/isa-def.h \
     $(wildcard include/config/rv64.h) \
 
-/home/admin/ics2023/nemu/build/obj-riscv32-nemu-interpreter/src/isa/riscv32/inst.o: $(deps_/home/admin/ics2023/nemu/build/obj-riscv32-nemu-interpreter/src/isa/riscv32/inst.o)
+/home/rookiemaster/ics2023/nemu/build/obj-riscv32-nemu-interpreter/src/isa/riscv32/inst.o: $(deps_/home/rookiemaster/ics2023/nemu/build/obj-riscv32-nemu-interpreter/src/isa/riscv32/inst.o)
 
-$(deps_/home/admin/ics2023/nemu/build/obj-riscv32-nemu-interpreter/src/isa/riscv32/inst.o):
+$(deps_/home/rookiemaster/ics2023/nemu/build/obj-riscv32-nemu-interpreter/src/isa/riscv32/inst.o):
